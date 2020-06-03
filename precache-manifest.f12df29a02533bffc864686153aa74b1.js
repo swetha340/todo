bd8c4ebe32cffbac097a8434d0dae96a0b68ee37
Pradeep Sunny <19pradeepsunnyc19@gmail.com>
2020-06-03 11:57:47 -0400

    Updates

---
+++ precache-manifest.f12df29a02533bffc864686153aa74b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9dc883f0c8676362e2f4856533382da",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "31cf72d2fa2373fe3bbf",
+    "url": "/todo/static/css/main.2fada55b.chunk.css"
+  },
+  {
+    "revision": "9ead8cb3b43037d5e1f8",
+    "url": "/todo/static/js/2.9fca414f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.9fca414f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31cf72d2fa2373fe3bbf",
+    "url": "/todo/static/js/main.76e0f971.chunk.js"
+  },
+  {
+    "revision": "42e23f9118876525bea8",
+    "url": "/todo/static/js/runtime-main.0d77e128.js"
+  }
+]);
